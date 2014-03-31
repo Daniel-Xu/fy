08e4886cc82b3afb00c4f0df061c9612a8f467fe
Daniel-Xu <xwd.forever@gmail.com>
2014-03-31 14:55:08 -0400

    change the structure:
    index.js exports the command that is needed by "./bin/{command_name}"

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-#!/usr/bin/env node
-
 var http = require("http")
 var word = process.argv[2]
 var url = "http://fanyi.youdao.com/openapi.do?keyfrom=forum3&key=1268771022&type=data&doctype=json&version=1.1&q=" + word
 
-var commander = require("commander")
+var program = require('commander')
 
-http.get(url, function(res){
-    var con = ""
-    res.setEncoding('utf8')
-    res.on('data', function(chunk){
-        con += chunk
+program
+    .command("fy")
+    .version('0.0.1')
+    .parse(process.argv)
+    //.option('-')
+
+module.exports = function() {
+    http.get(url, function(res){
+        var con = ""
+        res.setEncoding('utf8')
+        res.on('data', function(chunk){
+            con += chunk
+        })
+
+        res.on('end', function(){
+            console.log(con)
+        })
     })
 
-    res.on('end', function(){
-        console.log(con)
-    })
-})
+}
